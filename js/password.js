b7b69365d820c9014454449b6da1d51caf8a0be6
QCoinUser <85304261+qcoinfund@users.noreply.github.com>
2021-06-15 16:41:17 -0400

    made enter button work on password submit

--- js/password.js
+++ js/password.js
@@ -1,42 +1,24 @@
 const passwordButton = document.getElementById("password_button");
-
+const passwordInput = document.getElementById("enter_password");
 
 passwordButton.addEventListener("click", passwordCheck);
+passwordInput.addEventListener('keyup', function(event) {
+    if (event.code === 'Enter')
+    {
+        event.preventDefault();
+        passwordCheck(event);
+    }
+});
 
 function passwordCheck(event)
 {
-	const passwordInput = document.getElementById("enter_password").value;
-	if (passwordInput)
+	var password = passwordInput.value;
+	if (password)
 	{
-		var md5Hash = md5(passwordInput);
+		var md5Hash = md5(password);
 		if (md5Hash == "31f2df3f026bc2f6af00134eb51ab667")
-			window.location.href=passwordInput+".html";
+			window.location.href=password+".html";
 		else
 			alert("Sorry you are not authorized to buy in our pre-sale");
 	}
 }
-
-window.addEventListener('DOMContentLoaded', event => {
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            offset: 74,
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
-    });
-});
